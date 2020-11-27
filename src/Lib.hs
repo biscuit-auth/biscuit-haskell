95fa0dd9f63f4aebad4a2eaaaa2fe65ad7b5aba6
Clement Delafargue <clement.delafargue@fretlink.com>
2020-11-27 16:12:15 +0100

    WIP bindings

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,6 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-
 module Lib
     ( someFunc
     ) where
-
-
-import Foreign.C
-import Foreign.Ptr (Ptr.nullPtr)
-
-foreign import ccall "" c_keypair_new :: CDouble -> CDouble
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
