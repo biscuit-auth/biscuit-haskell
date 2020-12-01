f2e6c77d07a28536caa9d4940d2a8967036fea76
Clement Delafargue <clement@delafargue.name>
2020-12-01 09:32:09 +0100

    WIP: try inline-c

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,7 @@ module Lib
 import           Biscuit
 import           Control.Monad          ((<=<))
 import           Control.Monad          (when)
+import           Data.Bifoldable        (bitraverse_)
 import           Data.ByteString        (ByteString)
 import qualified Data.ByteString        as BS
 import           Data.ByteString.Base64
@@ -13,42 +14,56 @@ import           Data.ByteString.Short
 import           Data.Foldable          (traverse_)
 import           System.Random
 
+import qualified Inline                 as I
+
 getSeed :: IO ByteString
 getSeed = do
   BS.pack <$> traverse (const randomIO) [0..31]
 
-printKeyPairAddr = flip withKeyPair print
-printPublicKeyAddr = flip withPublicKey print
-printBiscuitBuilderAddr = flip withBiscuitBuilder print
-printBiscuitAddr = flip withBiscuit print
+printKeyPairAddr = (putStrLn "KeyPair" >>) . flip withKeyPair print
+printPublicKeyAddr = (putStrLn "PublicKey" >>) . flip withPublicKey print
+printBiscuitBuilderAddr = (putStrLn "Builder" >>) . flip withBiscuitBuilder print
+printBiscuitAddr = (putStrLn "Biscuit" >>) . flip withBiscuit print
+printVerifierAddr = (putStrLn "Verifier" >>) . flip withVerifier print
 
 someFunc :: IO ()
-someFunc = do
-  -- kp      <- keyPairNew =<< getSeed
-  -- print . encodeBase64 =<< serializeKeyPair kp
+someFunc = c2hs
+
+c2hs :: IO ()
+c2hs = do
+  kp      <- keyPairNew =<< getSeed
+  print . encodeBase64 =<< serializeKeyPair kp
   kp <- maybe undefined id <$> deserializeKeyPair (decodeBase64Lenient "3W/koXfmhyHtiNAi+Y9V5EI9/fi8OrGobD5OISpf3A8=")
   printKeyPairAddr kp
-  -- pub     <- keyPairPublic kp
+  pub     <- keyPairPublic kp
   builder <- biscuitBuilder kp
-  bisc    <-  biscuitBuilderBuild builder =<< getSeed
-  printBiscuitAddr bisc
-  print =<< encodeBase64 <$> serialize bisc
-  {-
-  verif <- biscuitVerify bisc pub
-   bisc <- biscuitFrom $ decodeBase64Lenient "CjwIABIEZWZnaBIEeW9sbxICYWEaCgoICAQSBAgAEAcqHAoaCggICBIECAAQCRIOCAQSBAgAEAASBAgAEAcaINBXDBnzFZJHwJUlx40i9KYQbJSo5it3lT06hblNbx1DIkQKIBzjq7s4HRdA8v7fILHd2yJ5pYSTKh3af4z1wc1LdegZEiDE9WWYDcGS877S+DnDqezkQHCbsDr5Nkvz+upxZdrOBw==" {-
+  bisc' <- biscuitFrom $ decodeBase64Lenient "CjwIABIEZWZnaBIEeW9sbxICYWEaCgoICAQSBAgAEAcqHAoaCggICBIECAAQCRIOCAQSBAgAEAASBAgAEAcaINBXDBnzFZJHwJUlx40i9KYQbJSo5it3lT06hblNbx1DIkQKIBzjq7s4HRdA8v7fILHd2yJ5pYSTKh3af4z1wc1LdegZEiDE9WWYDcGS877S+DnDqezkQHCbsDr5Nkvz+upxZdrOBw=="
+  printBiscuitAddr bisc'
   res <- biscuitBuilderAddAuthorityFact builder "right(#efgh)"
+  print res
   when (not res) $ do
     print "fact error"
     print =<< getErrorMessage
-  printBiscuitBuilderAddr builder
   res' <- biscuitBuilderAddAuthorityCaveat builder "*yolo(#aa) <- right(#authority,#efgh)"
+  print res'
   when (not res') $ do
     print "caveat error"
     print =<< getErrorMessage
+  bisc    <-  biscuitBuilderBuild builder =<< getSeed
+  printBiscuitAddr bisc
+  print =<< encodeBase64 <$> serialize bisc
+  verif <- biscuitVerify bisc pub
+  printVerifierAddr verif
+  printBiscuitBuilderAddr builder
   res'' <- verifierAddCaveat verif "*right(#abcd) <- right(#authority,#efgh)"
   print res''
-  -}
   putStrLn =<< verifierPrint verif
   vRes <- verifierVerify verif
   print vRes
-  -}
+
+b64p :: ByteString -> IO ()
+b64p = print . encodeBase64
+
+inline :: IO ()
+inline = do
+  (bitraverse_ b64p b64p) =<< I.genKp =<< getSeed
