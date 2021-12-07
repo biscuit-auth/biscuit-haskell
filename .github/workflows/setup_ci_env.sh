867af67faeb9891e2a2d0f1a30a39aca203caa06
Clement Delafargue <clement@delafargue.name>
2021-12-08 00:31:49 +0100

    ci: remove system deps install
    libsodium and c2hs are not needed anymore

--- .github/workflows/setup_ci_env.sh
+++ .github/workflows/setup_ci_env.sh
@@ -6,21 +6,16 @@ CI_OS=$(uname -s)
 
 install_deps_linux() {
   echo "Setting up the environment for linux"
-  sudo apt-get update && sudo apt install -y libsodium-dev
-  echo "$HOME/.ghcup/bin" >> "$GITHUB_PATH"
-  echo "HOME/.cabal/bin" >> "$GITHUB_PATH"
-  echo "HOME/.local/bin" >> "$GITHUB_PATH"
+  echo "${HOME}/.ghcup/bin" >> "$GITHUB_PATH"
+  echo "${HOME}/.cabal/bin" >> "$GITHUB_PATH"
+  echo "${HOME}/.local/bin" >> "$GITHUB_PATH"
 }
 
 install_deps_darwin() {
   echo "Setting up the environment for macOS"
-  brew update
-  brew install libsodium
 }
 
 case $CI_OS in
   Linux) install_deps_linux;;
   Darwin) install_deps_darwin;;
 esac
-
-cabal install c2hs
