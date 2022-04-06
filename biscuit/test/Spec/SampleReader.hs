fe00852bc54635514edc912d1f86be2116afaa3d
Clement Delafargue <clement@delafargue.name>
2022-04-07 01:05:54 +0200

    3rd-party: generate test samples

--- biscuit/test/Spec/SampleReader.hs
+++ biscuit/test/Spec/SampleReader.hs
@@ -7,10 +7,10 @@
 {-# LANGUAGE LambdaCase         #-}
 {-# LANGUAGE NamedFieldPuns     #-}
 {-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE QuasiQuotes        #-}
 {-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE TypeApplications   #-}
 module Spec.SampleReader where
-
-import           Debug.Trace
 
 import           Control.Arrow                 ((&&&))
 import           Control.Lens                  ((^?))
@@ -18,7 +18,8 @@ import           Control.Monad                 (join, void, when)
 import           Data.Aeson
 import           Data.Aeson.Lens               (key)
 import           Data.Aeson.Types              (typeMismatch, unexpected)
-import           Data.Attoparsec.Text          (parseOnly)
+import           Data.Attoparsec.Text          (endOfInput, parseOnly,
+                                                skipSpace)
 import           Data.Bifunctor                (Bifunctor (..))
 import           Data.ByteString               (ByteString)
 import qualified Data.ByteString               as BS
@@ -28,15 +29,17 @@ import           Data.Foldable                 (traverse_)
 import           Data.List.NonEmpty            (NonEmpty (..), toList)
 import           Data.Map.Strict               (Map)
 import qualified Data.Map.Strict               as Map
-import           Data.Maybe                    (isJust, isNothing)
+import           Data.Maybe                    (fromJust, isJust, isNothing)
 import           Data.Text                     (Text, pack, unpack)
 import           Data.Text.Encoding            (decodeUtf8, encodeUtf8)
+import           Data.Traversable              (for)
 import           GHC.Generics                  (Generic)
 
 import           Test.Tasty                    hiding (Timeout)
 import           Test.Tasty.HUnit
 
 import           Auth.Biscuit
+import           Auth.Biscuit.Datalog.AST      (renderAuthorizer, renderBlock)
 import           Auth.Biscuit.Datalog.Executor (ExecutionError (..),
                                                 ResultError (..))
 import           Auth.Biscuit.Datalog.Parser   (authorizerParser, blockParser)
@@ -45,10 +48,10 @@ import           Auth.Biscuit.Token
 getB :: ParsedSignedBlock -> Block
 getB ((_, b), _, _, _) = b
 
-getAuthority :: Biscuit OpenOrSealed Verified -> Block
+getAuthority :: Biscuit p Verified -> Block
 getAuthority = getB . authority
 
-getBlocks :: Biscuit OpenOrSealed Verified -> [Block]
+getBlocks :: Biscuit p Verified -> [Block]
 getBlocks = fmap getB . blocks
 
 instance FromJSON SecretKey where
@@ -58,6 +61,9 @@ instance FromJSON SecretKey where
         notSk = typeMismatch "Ed25519 secret key" (String t)
     maybe notSk pure res
 
+instance ToJSON SecretKey where
+  toJSON = toJSON . decodeUtf8 . serializeSecretKeyHex
+
 instance FromJSON PublicKey where
   parseJSON = withText "Ed25519 public key" $ \t -> do
     let bs = encodeUtf8 t
@@ -65,17 +71,23 @@ instance FromJSON PublicKey where
         notPk = typeMismatch "Ed25519 public key" (String t)
     maybe notPk pure res
 
+instance ToJSON PublicKey where
+  toJSON = toJSON . decodeUtf8 . serializePublicKeyHex
+
 instance FromJSON Authorizer where
   parseJSON = withText "authorizer" $ \t -> do
     let res = parseAuthorizer t
         notAuthorizer e = typeMismatch e (String t)
     either notAuthorizer pure res
 
+instance ToJSON Authorizer where
+  toJSON = toJSON . renderAuthorizer
+
 parseAuthorizer :: Text -> Either String Authorizer
 parseAuthorizer = parseOnly authorizerParser
 
 parseBlock :: Text -> Either String Block
-parseBlock = parseOnly blockParser
+parseBlock = parseOnly (blockParser <* skipSpace <* endOfInput)
 
 data SampleFile a
   = SampleFile
@@ -84,7 +96,7 @@ data SampleFile a
   , testcases        :: [TestCase a]
   }
   deriving stock (Eq, Show, Generic, Functor, Foldable, Traversable)
-  deriving anyclass FromJSON
+  deriving anyclass (FromJSON, ToJSON)
 
 data RustResult e a
   = Err e
@@ -100,6 +112,10 @@ instance (FromJSON e, FromJSON a) => FromJSON (RustResult e a) where
    parseJSON = genericParseJSON $
      defaultOptions { sumEncoding = ObjectWithSingleField }
 
+instance (ToJSON e, ToJSON a) => ToJSON (RustResult e a) where
+   toJSON = genericToJSON $
+     defaultOptions { sumEncoding = ObjectWithSingleField }
+
 type RustError = Value
 
 data ValidationR
@@ -109,8 +125,7 @@ data ValidationR
   , authorizer_code :: Authorizer
   , revocation_ids  :: [Text]
   } deriving stock (Eq, Show, Generic)
-    deriving anyclass FromJSON
-
+    deriving anyclass (FromJSON, ToJSON)
 
 checkResult :: Show a
             => (a -> RustError -> Assertion)
@@ -132,7 +147,7 @@ data TestCase a
   , validations :: Map String ValidationR
   }
   deriving stock (Eq, Show, Generic, Functor, Foldable, Traversable)
-  deriving anyclass FromJSON
+  deriving anyclass (FromJSON, ToJSON)
 
 data BlockDesc
   = BlockDesc
@@ -140,7 +155,7 @@ data BlockDesc
   , code    :: Text
   }
   deriving stock (Eq, Show, Generic)
-  deriving anyclass FromJSON
+  deriving anyclass (FromJSON, ToJSON)
 
 data WorldDesc
   =  WorldDesc
@@ -150,7 +165,7 @@ data WorldDesc
   , policies :: [Text]
   }
   deriving stock (Eq, Show, Generic)
-  deriving anyclass FromJSON
+  deriving anyclass (FromJSON, ToJSON)
 
 readBiscuits :: SampleFile FilePath -> IO (SampleFile (FilePath, ByteString))
 readBiscuits =
@@ -158,7 +173,7 @@ readBiscuits =
 
 readSamplesFile :: IO (SampleFile (FilePath, ByteString))
 readSamplesFile = do
-  Just f <- decodeFileStrict' "test/samples/v2/samples.json"
+  f <- either fail pure =<< eitherDecodeFileStrict' "test/samples/v2/samples-new.json"
   readBiscuits f
 
 checkTokenBlocks :: (String -> IO ())
@@ -258,9 +273,6 @@ runTests step = do
   SampleFile{..} <- readSamplesFile
   traverse_ (processTestCase step root_public_key) testcases
 
-specs :: TestTree
-specs = testCaseSteps "Biscuit samples - compliance checks" runTests
-
 mkTestCase :: PublicKey -> TestCase (FilePath, ByteString) -> TestTree
 mkTestCase root_public_key tc@TestCase{filename} =
   testCaseSteps (fst filename) (\step -> processTestCase step root_public_key tc)
@@ -270,3 +282,92 @@ getSpecs = do
   SampleFile{..} <- readSamplesFile
   pure $ testGroup "Biscuit samples - compliance checks"
        $ mkTestCase root_public_key <$> testcases
+mkTestCaseFromBiscuit
+  :: String
+  -> FilePath
+  -> Biscuit Open Verified
+  -> [(String, Authorizer)]
+  -> IO (TestCase FilePath)
+mkTestCaseFromBiscuit title filename biscuit authorizers = do
+  let mkBlockDesc :: Block -> BlockDesc
+      mkBlockDesc b = BlockDesc
+        { code = renderBlock b
+        , symbols = []
+        }
+      mkValidation :: Authorizer -> IO ValidationR
+      mkValidation authorizer = do
+        Right success <- authorizeBiscuit biscuit authorizer
+        pure ValidationR
+          { world = Just $ WorldDesc
+             { facts = []
+             , rules = []
+             , checks = []
+             , policies = []
+             }
+          , result = Ok 0
+          , authorizer_code = authorizer
+          , revocation_ids = decodeUtf8 . Hex.encode <$> toList (getRevocationIds biscuit)
+          }
+  BS.writeFile ("test/samples/v2/" <> filename) (serialize biscuit)
+  let token = mkBlockDesc <$> getAuthority biscuit :| getBlocks biscuit
+  validations <- Map.fromList <$> traverse (traverse mkValidation) authorizers
+
+  pure TestCase{..}
+
+addCases :: [(String, FilePath, Biscuit Open Verified, Authorizer)]
+         -> IO ()
+addCases cases = do
+  f <- either fail pure =<< eitherDecodeFileStrict' "test/samples/v2/samples.json"
+  newCases <- for cases $ \(name, path, biscuit, auth) -> do
+    mkTestCaseFromBiscuit name path biscuit [("", auth)]
+  let newF = f { testcases = testcases f <> newCases }
+  putStrLn "writing new"
+  encodeFile "test/samples/v2/samples-new.json" newF
+
+generateCases :: IO ()
+generateCases = do
+  putStrLn "generating new"
+  SampleFile{root_private_key} <- either fail pure =<< eitherDecodeFileStrict' @(SampleFile FilePath) "test/samples/v2/samples.json"
+  let (eSkOne, ePkOne) = (id &&& toPublic) $ fromJust $ parseSecretKeyHex "0932c942ef3535daac4b34eaff9ab6a848b730adbb49dfb24714d1f67c26a49b"
+  let (eSkTwo, ePkTwo) = (id &&& toPublic) $ fromJust $ parseSecretKeyHex "c520e19539e88fea4ccb388ad242ac962cafe8158a3a9da04c17159281c668fc"
+  let (eSkThree, ePkThree) = (id &&& toPublic) $ fromJust $ parseSecretKeyHex "d547622fe6f3afeb83200b5dcc4a8efcb8fd53cde9833da313a9bdf6c12a035c"
+  b1 <- do
+      b <- mkBiscuit root_private_key [block|right("read"); check if group("admin") trusting ${ePkOne};|]
+      addSignedBlock eSkOne [block|group("admin"); check if right("read"); |] b
+  let a1 = [authorizer|allow if true;|]
+  b2 <- do
+      b <- mkBiscuit root_private_key [block|
+                   query(0);
+                   check if true trusting previous, ${ePkOne};
+               |]
+      b' <- addSignedBlock eSkOne [block|
+                   query(1);
+                   query(1,2) <- query(1), query(2) trusting ${ePkTwo} ;
+                   check if query(2), query(3) trusting ${ePkTwo};
+                   check if query(1) trusting ${ePkOne};
+                 |] b
+      b'' <- addSignedBlock eSkTwo [block|
+                   query(2);
+                   check if query(2), query(3) trusting ${ePkTwo};
+                   check if query(1) trusting ${ePkOne};
+                 |] b'
+      b''' <- addSignedBlock eSkTwo [block|
+                   query(3);
+                   check if query(2), query(3) trusting ${ePkTwo};
+                   check if query(1) trusting ${ePkOne};
+                 |] b''
+      addBlock [block|
+           query(4);
+           check if query(2) trusting ${ePkTwo};
+           check if query(4) trusting ${ePkThree};
+         |] b'''
+  let a2 = [authorizer|
+     check if query(1,2) trusting ${ePkOne}, ${ePkTwo};
+     deny if query(3);
+     deny if query(1,2);
+     allow if true;
+  |]
+  addCases
+    [ ("third-party block", "test24_third_party.bc", b1, a1)
+    , ("third-party blocks keys", "test25_third_party_keys.bc", b2, a2)
+    ]
