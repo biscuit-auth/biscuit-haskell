1939880febc34770016efc27c20d9e9986d48443
Clément Delafargue <clement@delafargue.name>
2023-05-18 13:47:45 +0200

    biscuit-wai: allow configuring parsing & extraction

--- biscuit-wai/src/Network/Wai/Middleware/Biscuit.hs
+++ biscuit-wai/src/Network/Wai/Middleware/Biscuit.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Wai.Middleware.Biscuit (parseBiscuit, getBiscuit) where
 
 import           Auth.Biscuit       (Biscuit, OpenOrSealed, ParseError,
                                      PublicKey, Verified, parseB64)
+import           Control.Monad      ((<=<))
+import           Data.Bifunctor     (first)
 import           Data.ByteString    (ByteString)
 import qualified Data.ByteString    as BS
 import qualified Data.List          as List
@@ -23,25 +26,40 @@ getBiscuit :: Request -> Maybe (Biscuit OpenOrSealed Verified)
 getBiscuit = Vault.lookup biscuitKey . vault
 
 parseBiscuit :: PublicKey -> Middleware
-parseBiscuit publicKey app req sendResponse = case defaultExtractToken req of
-    Nothing -> sendResponse =<< defaultHandleError NoToken
-    Just s -> case defaultParseToken publicKey s of
-      Left e -> sendResponse =<< defaultHandleError (ParseError e)
-      Right t -> do
+parseBiscuit = parseBiscuitWith . defaultExtractionConfig
+
+parseBiscuitWith :: ExtractionConfig e -> Middleware
+parseBiscuitWith config app req sendResponse = do
+  let ExtractionConfig{extractToken,parseToken,handleError} = config
+      onError = sendResponse <=< handleError
+      forward t = do
          let newVault = Vault.insert biscuitKey t (vault req)
          app req { vault = newVault } sendResponse
+  eBiscuit <- either (pure . Left) parseToken =<< extractToken req
+  either onError forward eBiscuit
+
+data ExtractionConfig e
+  = ExtractionConfig
+  { extractToken :: Request -> IO (Either e ByteString)
+  , parseToken   :: ByteString -> IO (Either e (Biscuit OpenOrSealed Verified))
+  , handleError  :: e -> IO Response
+  }
 
 data BiscuitError
   = NoToken
   | ParseError ParseError
 
+defaultExtractionConfig :: PublicKey -> ExtractionConfig BiscuitError
+defaultExtractionConfig publicKey = ExtractionConfig
+  { extractToken = pure . maybe (Left NoToken) Right . defaultExtractToken
+  , parseToken = pure . Data.Bifunctor.first ParseError . parseB64 publicKey
+  , handleError = defaultHandleError
+  }
+
 defaultExtractToken :: Request -> Maybe ByteString
 defaultExtractToken req = do
   (_, authHeader) <- List.find ((== hAuthorization) . fst) $ requestHeaders req
   BS.stripPrefix "Bearer " authHeader
-
-defaultParseToken :: PublicKey -> ByteString -> Either ParseError (Biscuit OpenOrSealed Verified)
-defaultParseToken = parseB64
 
 defaultHandleError :: BiscuitError -> IO Response
 defaultHandleError = \case
